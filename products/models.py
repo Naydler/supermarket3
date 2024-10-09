4dfb8bedcb75302d8ebede54555182633bf05b0d
Naydler <ilsalbesatrujillogerard@gmail.com>
2024-10-09 18:41:33 +0200

    create model Company

--- products/models.py
+++ products/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+# class Shop(models.Model):
+#     name = models.CharField(max_length=255)
+    
+# class Product(models.Model):
+#     shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
+#     name = models.CharField(max_length=255)
+#     ean = models.CharField(max_length=13)
+#     price_of_buy = models.DecimalField(max_digits=10, decimal_places=2)
+#     price_of_sell = models.DecimalField(max_digits=10, decimal_places=2)
+#     stock_warning = models.IntegerField()
+#     date = models.DateTimeField(auto_now_add=True)
+#     image = models.URLField()
+#     category = models.IntegerField()
