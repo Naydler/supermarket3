d6424414492343d0e6386cb4e6b725b00c6565ad
Naydler <ilsalbesatrujillogerard@gmail.com>
2024-10-12 00:16:58 +0200

    Ignore migrations in .gitignore and create privilege for employees

--- employee/models.py
+++ employee/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.hashers import make_password
 class Employee (models.Model):
     shopid = models.ForeignKey('shop.Shop', on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
-    privilege = models.CharField(max_length=255)
+    privilege = models.ForeignKey('privileges.Privilege', on_delete=models.SET_NULL, null=True, related_name='employees')
     login = models.CharField(max_length=255)
     password = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
@@ -14,7 +14,7 @@ class Employee (models.Model):
         return self.name
     
     def save(self, *args, **kwargs):
-        # Check if the password is already hashed
+        # Check if the password is already hashed   
         if not self.password.startswith('pbkdf2_'): 
             self.password = make_password(self.password)
         super(Employee, self).save(*args, **kwargs)
--- supermarket/settings.py
+++ supermarket/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "shop",
     "employee",
     "product",
+    "privileges",
 ]
 
 MIDDLEWARE = [
