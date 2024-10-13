9bcef680e9c7579981fae2b5d6c3bfa0295e0f53
vane <vanebq5@gmail.com>
2024-10-13 18:50:58 +0200

    guardado antes de kan13

--- supplier/models.py
+++ supplier/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # El campo default_price = models.DecimalField, que estaba en el diagrama no lo he añadido, creo que es un
 #campo que deberia ir en producto, aqui añadiria el campo "si tenemos o no descuentos -->discount"
+
 class Supplier(models.Model):
     name = models.CharField(max_length=200)
     web_link = models.CharField(max_length=200)
@@ -11,5 +12,6 @@ class Supplier(models.Model):
     nif = models.CharField(max_length=15)
 
     def __str__(self):
-        return self.name
+        return f"{self.name}(ID:{self.id})"
+
 
