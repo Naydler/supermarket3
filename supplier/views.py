4c0f4a6ddb2290154195d579f5415c9a05cb8c99
vane <vanebq5@gmail.com>
2024-10-14 17:12:42 +0200

    terminada primera implementación de supplier

--- supplier/views.py
+++ supplier/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+
+from .models import Supplier
+from .serializers import SupplierSerializer
+from rest_framework import generics
+
 
 # Create your views here.
+
+class SupplierListCreateView(generics.ListCreateAPIView):
+    queryset= Supplier.objects.all()
+    serializer_class = SupplierSerializer
+
+class SupplierRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Supplier.objects.all()
+    serializer_class = SupplierSerializer
