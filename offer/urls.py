0c2200a73859a8a0d80064276a0ba0d5716c60b7
Naydler <ilsalbesatrujillogerard@gmail.com>
2024-10-15 16:29:36 +0200

    Add API endpoints to get offers by shop ID and employee ID

--- offer/urls.py
+++ offer/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import OfferListCreateView, OfferRetrieveUpdateDestroyView
+from .views import *
 
 
 urlpatterns = [
     path('create', OfferListCreateView.as_view(), name='offer-list-create'),
     path('update/<int:pk>/', OfferRetrieveUpdateDestroyView.as_view(), name='offer-detail'),
+
+    path('api/byshop/<int:shopid>/', get_offers_by_shopid, name='offer-by-shopid'),
+    path('api/byemployee/<int:employeeid>/', get_offer_by_employee, name='offer-by-employee'),
+
 ]
--- offer/views.py
+++ offer/views.py
@@ -1,6 +1,8 @@
 from rest_framework import generics
 from .models import Offer
 from .serializers import OfferSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 # Create your views here.
 class OfferListCreateView(generics.ListCreateAPIView):
@@ -10,3 +12,30 @@ class OfferListCreateView(generics.ListCreateAPIView):
 class OfferRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Offer.objects.all()
     serializer_class = OfferSerializer
+
+
+
+@api_view(['GET'])
+def get_offers_by_shopid(request, shopid):
+    offers = Offer.objects.filter(idShop_id=shopid)
+
+    # Check if there is only one offer
+    if offers.count() == 1:
+        # if there is only one, use many=False
+        serializer = OfferSerializer(offers.first())
+    else:
+        # if there is more than one, use many=True
+        serializer = OfferSerializer(offers, many=True)
+
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def get_offer_by_employee(request, employeeid):
+    offers = Offer.objects.filter(idEmployee_id=employeeid)
+
+    if offers.count() == 1:
+        serializer = OfferSerializer(offers.first())
+    else:
+        serializer = OfferSerializer(offers, many=True)
+
+    return Response(serializer.data)
