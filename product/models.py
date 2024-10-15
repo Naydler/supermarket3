1b87bc9ea52c4b52a36ba6d19b3a125586ac91d4
Naydler <ilsalbesatrujillogerard@gmail.com>
2024-10-15 18:31:10 +0200

    Add stock field to Product model

--- product/models.py
+++ product/models.py
@@ -13,10 +13,12 @@ class Product(models.Model):
     price_of_buy = models.DecimalField(max_digits=10, decimal_places=2)
     price_of_sell = models.DecimalField(max_digits=10, decimal_places=2)
     stock_warning = models.IntegerField()
+    stock = models.IntegerField()
     date = models.DateTimeField(auto_now_add=True)
     image = models.URLField(blank=True)
     ivaCategoryId = models.ForeignKey('ivaCategory.IvaCategory', default=None, on_delete=models.PROTECT)
     id_supplier = models.ForeignKey('supplier.Supplier',default=None,on_delete=models.PROTECT )
+
 
     
     def __str__(self):
