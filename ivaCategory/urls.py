1c780b694cfa65e0c460de3cef4915109f9ae751
Naydler <ilsalbesatrujillogerard@gmail.com>
2024-10-17 21:22:24 +0200

    Refactor IvaCategory URLs and views
    - Added new API endpoints for retrieving IvaCategory objects based on different criteria
    - Updated the URLs in the `ivaCategory/urls.py` file to include the new API endpoints
    - Created corresponding view functions in the `ivaCategory/views.py` file for the new API endpoints
    - Imported necessary modules and decorators for the new view functions

--- ivaCategory/urls.py
+++ ivaCategory/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
-from .views import IvaCategoryListCreateView,IvaCategoryRetrieveUpdateDestroyView
+from .views import *
 
 urlpatterns = [
     path('create/', IvaCategoryListCreateView.as_view(), name='ivaCategory-list-create'),
     path('update/<int:pk>/', IvaCategoryRetrieveUpdateDestroyView.as_view(), name='`ivaCategory`-detail'),
+
+    path('api/getallivacategory/', get_all_ivacategory, name='`ivaCategory`-list'),
+    path('api/getivacategorybyid/<int:ivacategoryid>/', get_ivacategory_by_id, name='`ivaCategory`-detail'),
+    path('api/getivacategorybyname/<str:ivacategoryname>/', get_ivacategory_by_name, name='`ivaCategory`-detail'),
+    path('api/getivacategorybypercentage/<str:ivacategorypercentage>/', get_ivacategory_by_percentage, name='`ivaCategory`-detail'),
+    path('api/getivacategorybypercentagegreaterthan/<str:ivacategorypercentage>/', get_ivacategory_by_percentage_greater_than, name='`ivaCategory`-detail'),
+    path('api/getivacategorybypercentagelessthan/<str:ivacategorypercentage>/', get_ivacategory_by_percentage_less_than, name='`ivaCategory`-detail'),
+    
+
 ]
--- ivaCategory/views.py
+++ ivaCategory/views.py
@@ -1,6 +1,8 @@
 from rest_framework import generics
 from .models import IvaCategory
 from .serializers import IvaCategorySerializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 # Create your views here.
 class IvaCategoryListCreateView(generics.ListCreateAPIView):
@@ -10,3 +12,68 @@ class IvaCategoryListCreateView(generics.ListCreateAPIView):
 class IvaCategoryRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = IvaCategory.objects.all()
     serializer_class = IvaCategorySerializer
+
+
+@api_view(['GET'])
+def get_all_ivacategory(request):
+    iva_category_list = IvaCategory.objects.all()
+    serializer = IvaCategorySerializer(iva_category_list, many=True)
+    
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def get_ivacategory_by_id(request, ivacategoryid):
+    iva_category = IvaCategory.objects.filter(id=ivacategoryid)
+    
+    if iva_category.count() == 1:
+        serializer = IvaCategorySerializer(iva_category.first())
+    else:
+        serializer = IvaCategorySerializer(iva_category, many=True)
+    
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def get_ivacategory_by_name(request, ivacategoryname):
+    iva_category = IvaCategory.objects.filter(name=ivacategoryname)
+    
+    if iva_category.count() == 1:
+        serializer = IvaCategorySerializer(iva_category.first())
+    else:
+        serializer = IvaCategorySerializer(iva_category, many=True)
+    
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def get_ivacategory_by_percentage(request, ivacategorypercentage):
+    iva_category = IvaCategory.objects.filter(percentage=ivacategorypercentage)
+    
+    if iva_category.count() == 1:
+        serializer = IvaCategorySerializer(iva_category.first())
+    else:
+        serializer = IvaCategorySerializer(iva_category, many=True)
+    
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def get_ivacategory_by_percentage_greater_than(request, ivacategorypercentage):
+    iva_category = IvaCategory.objects.filter(percentage__gt=ivacategorypercentage)
+    
+    if iva_category.count() == 1:
+        serializer = IvaCategorySerializer(iva_category.first())
+    else:
+        serializer = IvaCategorySerializer(iva_category, many=True)
+    
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def get_ivacategory_by_percentage_less_than(request, ivacategorypercentage):
+    iva_category = IvaCategory.objects.filter(percentage__lt=ivacategorypercentage)
+    
+    if iva_category.count() == 1:
+        serializer = IvaCategorySerializer(iva_category.first())
+    else:
+        serializer = IvaCategorySerializer(iva_category, many=True)
+    
+    return Response(serializer.data)
+
+
