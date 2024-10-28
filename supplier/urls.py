dfa11da6de8efa9b0902e88cd4e4e23e3ba7b0e2
vane <vanebq5@gmail.com>
2024-10-28 21:10:17 +0100

    creadas 2 apis propias para buscar por nif y por nombre

--- supplier/urls.py
+++ supplier/urls.py
@@ -1,8 +1,13 @@
 
 from django.urls import path,include
-from .views import SupplierListCreateView, SupplierRetrieveUpdateDestroyView
+from .views import (SupplierListCreateView, 
+                    SupplierRetrieveUpdateDestroyView,
+                    search_suppliers_by_name,
+                    search_suppliers_by_nif)
 
 urlpatterns = [
     path('create/', SupplierListCreateView.as_view(), name='supplier-list-create'),
     path('update/<int:pk>/', SupplierRetrieveUpdateDestroyView.as_view(), name='supplier-detail'),
+    path('search-by-name/', search_suppliers_by_name, name='search-suppliers-by-name'),
+    path('search-by-nif/', search_suppliers_by_nif, name='search-suppliers-by-nif'),
 ]
